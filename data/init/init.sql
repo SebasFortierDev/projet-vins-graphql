1c964c4b8f82bd0e327fb718daec1392134fa3e6
Sébastien Fortier <95516857+SebasFortierDev@users.noreply.github.com>
2022-07-01 17:25:27 -0400

    Commit initial | Docker fonctionnel

---
+++ data/init/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `vins` (
+    `id` varchar(20) NOT NULL,
+    `name` varchar(100) NOT NULL,
+    `year` int NOT NULL,
+    `grade` varchar(100) NOT NULL
+)
+
+ENGINE=InnoDB DEFAULT
+CHARSET=utf8mb4;
