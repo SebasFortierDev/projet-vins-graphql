ae0c787d63c1c72d97e23fd8a0da9f832e2629f7
Sébastien Fortier <95516857+SebasFortierDev@users.noreply.github.com>
2022-07-02 12:38:51 -0400

    Ajout de variables JS dans la query CreateVin

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
-
 const vinContainer = document.getElementById("vinContainer");
+document.getElementById("createVinButton").addEventListener("click", createVin);
 
 const instanceAxios = axios.create({
     baseURL: "http://localhost:3000",
@@ -9,14 +9,54 @@ const instanceAxios = axios.create({
 function getVins() {
     instanceAxios.post('/graphql', {
         query: `query {
-                    getVin(id: "b011735eae2b62daeede") {
+                    getVins {
                         id
                         name
+                        year
+                        grade
                     }
                 }`,
     }).then(response => {
-        console.log(response.data.data.getVin);
+        showVins(response.data.data.getVins);
     }).catch(error => console.log('error', error))
+}
+
+function createVin() {
+    const vinName = "Salut!!"
+
+    instanceAxios.post('/graphql', {
+        query: `mutation CreateVin($vinName: String!) {
+                    createVin(input: {
+                        name: $vinName
+                        year: 1995
+                        grade: 5
+                      }) {
+                            id
+                            name
+                          }
+                    }`,
+        variables: {vinName: vinName},
+    }).then(response => {
+        getVins(response)
+    }).catch(error => console.log('error', error))
+}
+
+function showVins(vins) {
+    for (const vin of vins) {
+        let tagName = document.createElement("p");
+        tagName.innerText = "Nom du vin: " + vin.name
+
+        let tagYear = document.createElement("p");
+        tagYear.innerText = "Année: " + vin.year
+
+        let tagGrade = document.createElement("p");
+        tagGrade.innerText = "Note: " + vin.grade + "/" + "5"
+
+        vinContainer.append(tagName);
+        vinContainer.append(tagYear);
+        vinContainer.append(tagGrade);
+        vinContainer.append(document.createElement("br"))
+    }
 }
 
 getVins();
